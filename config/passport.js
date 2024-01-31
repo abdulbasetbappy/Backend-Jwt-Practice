285dfc28196a88b38378fa61c87540b7bdd80932
abdulbasetbappy <abdulbasetbappy@hotmial.com>
2024-01-31 16:15:44 +0600

    Token Verification Done

--- config/passport.js
+++ config/passport.js
@@ -9,15 +9,17 @@ const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = process.env.SECRET_KEY;
 
-passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
-    User.findOne({id: jwt_payload.id}, function(err, user) {
-        if (err) {
-            return done(err, false);
-        }
+passport.use(new JwtStrategy(opts, async (jwt_payload, done) => {
+    try {
+        const user = await User.findOne({ _id: jwt_payload.id });
+        
         if (user) {
             return done(null, user);
         } else {
             return done(null, false);
         }
-    });
+    } catch (error) {
+        return done(error, false);
+    }
+    
 }));
